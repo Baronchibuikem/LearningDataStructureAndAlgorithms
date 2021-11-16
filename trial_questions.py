2b6fb5231165803117bdd64b0c29495699108a5f
baron <baronchibuike@gmail.com>
2021-11-16 09:59:19 -0800

    more questions

--- trial_questions.py
+++ trial_questions.py
@@ -35,9 +35,9 @@ def sum_of_scores_on_a_board(array: list) -> int:
     record = []
 
     for i in array:
-        if i.isdigit() or "-" in i:
-            record.append(int(i))
-        elif i == "+":
+        # if i.isdigit() or "-" in i:
+        #     record.append(int(i))
+        if i == "+":
             score = record[-1] + record[-2]
             record.append(score)
         elif i == "D":
@@ -45,10 +45,77 @@ def sum_of_scores_on_a_board(array: list) -> int:
             record.append(score)
         elif i == "C":
             record.pop()
+        else:
+            record.append(int(i))
+
     total_records = sum(record)
     return total_records
 
 
-for test in tests_data:
-    result = sum_of_scores_on_a_board(**test["input"])
-    print(result == test["output"])
+# for test in tests_data:
+#     result = sum_of_scores_on_a_board(**test["input"])
+#     print(result == test["output"])
+
+
+"""
+    Question.
+    Given an array of integers, return indices of the two numbers such that they add up to a 
+    specific target.
+
+    You may assume that each input would have exactly one solution and you may not use the same element 
+    twice
+"""
+
+# Solution
+# Given an array, get the indexes of the two elements that will add to the given target
+
+# inputs = [2, 7, 11, 16], target_number = 9
+# output = [0,1]
+
+# function scalford = def find_indexes(items:list, sum:int) -> list[int]
+
+# edge cases:
+#  empty list
+#  correct numbers occuring at the end of the array
+#  correct number occuring at the middle of the array
+#  correct number occuring at the end of the array
+#  correct indexs not found
+
+test_data = [
+    {"inputs": {"items": [], "summed": 9}, "output": [-1, -1]},
+    {"inputs": {"items": [2, 7, 11, 16], "summed": 9}, "output": [0, 1]},
+    {
+        "inputs": {"items": [8, 12, 13, 82, 40, 7, 11, 16, 24], "summed": 40},
+        "output": [5, 6],
+    },
+    {"inputs": {"items": [8], "summed": 9}, "output": [-1, -1]},
+    {
+        "inputs": {"items": [8, 12, 13, 82, 40, 7, 11, 16, 24], "summed": 19},
+        "output": [1, 2],
+    },
+]
+
+
+def find_index(items: list, summed: int) -> list[int]:
+    # sort the list
+    sorted_items = sorted(items)
+
+    if len(sorted_items) == 0 or len(sorted_items) == 1:
+        return [-1, -1]
+
+    for i in range(0, len(sorted_items) - 1):
+        if sorted_items[i] + sorted_items[i + 1] == summed:
+            return [
+                sorted_items.index(sorted_items[i]),
+                sorted_items.index(sorted_items[i + 1]),
+            ]
+
+        continue
+
+    # brute force
+
+
+# for test in test_data:
+#     result = find_index(**test["inputs"])
+#     print("<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>")
+#     print(result == test["output"])
